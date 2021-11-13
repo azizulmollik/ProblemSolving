12e892df61ffb5b539d7e69e4f65566dd872473d
Mollik <Mollik@DESKTOP-MOLLIK>
2021-11-13 19:20:22 +0600

    Add project files.

---
+++ CF_0001_Watermelon/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CF_0001_Watermelon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int w=0;
+            w= Convert.ToInt32(Console.ReadLine());
+
+            if(w>2 && w%2==0)
+                Console.WriteLine("YES");
+            else
+                Console.WriteLine("NO");
+
+        }
+    }
+}
